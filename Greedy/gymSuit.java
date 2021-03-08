9a36cdd342797123eb83170bc77ee8d558a67370
asd3638 <asd36384@gmail.com>
2021-03-08 17:42:28 +0900

    Greedy #1

---
+++ Greedy/gymSuit.java
@@ -0,0 +1,40 @@
+package Greedy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class gymSuit {
+    public static int solution(int n, int[] lost, int[] reserve) {
+        int answer = n - lost.length;
+
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if (lost[i] == reserve[j]) {
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    answer++;
+                    break;
+                }
+            }
+        }
+        
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if (((lost[i] - reserve[j]) == -1) || ((lost[i] - reserve[j]) == 1)) {
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    answer++;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        int[] lost = {1, 2, 3, 4, 5, 6, 7};
+        int[] reserve = {1, 2, 3};
+        System.out.println(solution(7, lost, reserve) + "");
+    }
+}
