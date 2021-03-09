0fabc21bd61af03fb33fb2cad689211dc6aa8340
asd3638 <asd36384@gmail.com>
2021-03-09 11:06:54 +0900

    before greedy #2

--- Greedy/joyStick.java
+++ Greedy/joyStick.java
@@ -1,22 +1,67 @@
 package Greedy;
 
-public class joyStick {
-    public 
+public class joyStick { 
     public static int solution(String name) {
         int answer = 0;
-        String betweenAnswer = "";
-        for (int i = 0; i < name.length(); i++) {
-            betweenAnswer += "A";
-        }
-        String between = name.substring(1, name.length() - 1);
-        if (between.equals(betweenAnswer)) {
+        int wIndex = 0;
 
+        while (true) {
+            int rIndex = wIndex + 1;
+            int rMove = 1;
+            int lMove = name.length() - 1;
+
+            answer += changeAlphabet(wIndex, name);
+            System.out.println("" + answer);
+
+            if (wIndex < name.length() - 1) {
+                if (name.charAt(wIndex + 1) == 'A') {
+                    lMove = wIndex;
+                    for (int i = name.length() - 1; i > wIndex; i--) {
+                        if (name.charAt(i) == 'A') {
+    
+                        } else {
+                            lMove++;
+                        }
+                    }
+                }
+            }
+            while (rIndex < name.length()) {
+                if (name.charAt(rIndex) == 'A') {
+                    rMove++;
+                    rIndex++;
+                } else {
+                    break;
+                }
+            }
+            if (rMove <= lMove) {
+                //right
+                wIndex += rMove;
+                answer += rMove;
+            } else {
+                //left
+                wIndex = name.length() - ((lMove - wIndex));
+                answer += lMove;
+            }
+            if(wIndex == name.length()) {
+                break;
+            }
         }
 
-        return answer;
+        return answer - 1;
+    }
+    public static int changeAlphabet(int i, String name) {
+        int up = name.charAt(i) - 'A';
+        int down = 'Z' - name.charAt(i) + 1;
+        
+        if(up > down) {
+            return down;
+        } else {
+            return up;
+        } 
     }
     public static void main(String[] args) {
-        String test = "abcd" * 5;
-        System.out.println(solution(test));
+        String name = "JEROEN";
+        int result = solution(name);
+        System.out.println(result + "");
     }
 }
