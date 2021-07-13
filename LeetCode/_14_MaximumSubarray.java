e3e0c627ae3dd23f3e3a955aaf9e47f2a0618508
jihye <asd36384@gmail.com>
2021-07-13 15:10:47 +0900

    solve leetcode14

--- LeetCode/_14_MaximumSubarray.java
+++ LeetCode/_14_MaximumSubarray.java
@@ -1,5 +1,20 @@
 package LeetCode;
 
 public class _14_MaximumSubarray {
+    public static int solution1(int[] nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
 
+        for(int i=1; i<nums.length; i++) {
+            currentSum = Math.max(nums[i]+currentSum, nums[i]);
+            maxSum = Math.max(currentSum, maxSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-2, -1};
+        System.out.println(solution1(nums));
+    }
 }
